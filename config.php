d1f48fad4894e409c88d152a4c10c215f88f5446
Ewelina Tokarz <ewelina.tokarz.programista@gmail.com>
2023-03-04 20:37:10 +0100

    Added email function and set_error_handler function

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'cms');
+define('DB_USER', 'cms_www');
+define('DB_PASS', 'QagNeDt!8v.3@j]q');
+
+define('SMTP_HOST', 'smtp.gmail.com');
+define('SMTP_USER', 'ewelinatokarz28@gmail.com');
+define('SMTP_PASS', '1994Autograf66575!386');
+
+define('SHOW_ERROR_DETAIL', true);
