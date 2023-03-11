849da227759d6379cf2c165140f7eeda199a98a9
Ewelina Tokarz <ewelina.tokarz.programista@gmail.com>
2023-03-11 17:43:45 +0100

    Fixing problems with contact section

--- config.php
+++ config.php
@@ -7,6 +7,10 @@ define('DB_PASS', 'QagNeDt!8v.3@j]q');
 
 define('SMTP_HOST', 'smtp.gmail.com');
 define('SMTP_USER', 'ewelinatokarz28@gmail.com');
-define('SMTP_PASS', 'password');
+define('SMTP_PASS', 'gvwyqzxybpvchbbh');
+
+define('CLIENTID', '{677989909552-fi43cn3q9fc4vj49sl9vm8apnncfhcln.apps.googleusercontent.com}');
+define('CLIENTSECRET', '{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}');
+define('REDIRECTURI', '{https://accounts.google.com/o/oauth2/v2/auth}');
 
 define('SHOW_ERROR_DETAIL', true);
